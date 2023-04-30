b9b1ed2fa394e3383e16284f5ae19c9c39fc5532
wandakim <nwd1583@naver.com>
2023-04-30 13:12:50 +0900

    Add filter header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
 import TodoList from './component/TodoList';
-
+import Header from './component/Header/Header';
+import { useState } from 'react';
+const filters = ['all', 'active', 'completed'];
 function App() {
+  const [filter, setFilter] = useState(filters[0]);
   return (
     <div>
-      <TodoList />
+      <Header filters={filters} filter={filter} onFilterChange={setFilter} />
+      <TodoList filter={filter} />
     </div>
   );
 }
--- src/component/TodoList.jsx
+++ src/component/TodoList.jsx
@@ -3,8 +3,11 @@ import { useState } from 'react';
 import AddTodo from './AddTodo';
 import Todo from './Todo/Todo';
 
-export default function TodoList() {
-  const [todos, setTodos] = useState([]);
+export default function TodoList({ filter }) {
+  const [todos, setTodos] = useState([
+    { id: '123', text: '장보기', status: 'active' },
+    { id: '124', text: '공부하기', status: 'active' },
+  ]);
   const handleAdd = (todo) => {
     setTodos([...todos, todo]);
   };
@@ -14,15 +17,15 @@ export default function TodoList() {
   const handleDelete = (deleted) => {
     setTodos(todos.filter((t) => t.id !== deleted.id));
   };
-
+  const filtered = getFilteredItems(todos, filter);
   return (
     <section>
       <ul>
-        {todos.map((item) => (
+        {filtered.map((item) => (
           <Todo
             key={item.id}
             todo={item}
-            onUpdate={handleUpdate}
+            onUpdate={handleUpdate} // checkbox
             onDelete={handleDelete}
           />
         ))}
@@ -30,4 +33,11 @@ export default function TodoList() {
       <AddTodo onAdd={handleAdd} />
     </section>
   );
+}
+
+function getFilteredItems(todos, filter) {
+  if (filter === 'all') {
+    return todos;
+  }
+  return todos.filter((todo) => todo.status === filter);
 }
