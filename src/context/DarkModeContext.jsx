d5f1f1df90b31d86ef8be780dbcfdc180c371ee2
wandakim <nwd1583@naver.com>
2023-05-06 21:50:04 +0900

    DarkMode: Context, local storage

--- src/context/DarkModeContext.jsx
+++ src/context/DarkModeContext.jsx
@@ -1,5 +1,6 @@
 import { createContext, useState } from 'react';
 import { useContext } from 'react';
+import { useEffect } from 'react';
 
 export const DarkModeContext = createContext();
 
@@ -9,6 +10,14 @@ export function DarkModeProvider({ children }) {
     setDarkMode(!darkMode);
     updateDarkMode(!darkMode);
   };
+  useEffect(() => {
+    const isDark =
+      localStorage.theme === 'dark' ||
+      (!('theme' in localStorage) &&
+        window.matchMedia('(prefers-color-scheme: dark)').matches);
+    setDarkMode(isDark);
+    updateDarkMode(isDark);
+  }, []);
   return (
     <DarkModeContext.Provider value={{ darkMode, toggleDarkMode }}>
       {children}
@@ -19,8 +28,10 @@ export function DarkModeProvider({ children }) {
 function updateDarkMode(darkMode) {
   if (darkMode) {
     document.documentElement.classList.add('dark');
+    localStorage.theme = 'dark';
   } else {
     document.documentElement.classList.remove('dark');
+    localStorage.theme = 'light';
   }
 }
 export const useDarkMode = () => useContext(DarkModeContext);
